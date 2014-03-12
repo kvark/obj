fe494ca69f58ace8b2981600324e930a3d07e2f1
Colin Sherratt <colin.sherratt@gmail.com>
2014-03-12 00:31:18 -0400

    fixed unit test

--- test.rs
+++ test.rs
@@ -5,13 +5,13 @@ use loader::Obj;
 #[test]
 fn load_teapot()
 {
-    let teapot = Obj::load(&Path::new("assets/teapot.obj"));
+    let teapot = Obj::load(&Path::new("assets/suzanne.obj"));
 
     assert!(teapot.is_some());
 
     let teapot = teapot.unwrap();
 
     for &(ref name, _, _) in teapot.objects.iter() {
-        assert!(name.as_slice() == "Teapot01");
+        assert!(name.as_slice() == "Suzanne");
     }
 }
